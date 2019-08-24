682f9e089818740945732c2d434ad7076ef77de4
Craiegg <48470972+Craiegg@users.noreply.github.com>
2019-08-24 18:55:12 +1000

    Add files via upload

---
+++ RoutingMap/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace RoutingMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataCustomer = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtProducts = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.showMapBtn = new System.Windows.Forms.Button();
+            this.showDistBtn = new System.Windows.Forms.Button();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.dataCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataCustomer
+            // 
+            this.dataCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataCustomer.Location = new System.Drawing.Point(572, 256);
+            this.dataCustomer.Name = "dataCustomer";
+            this.dataCustomer.RowHeadersWidth = 51;
+            this.dataCustomer.RowTemplate.Height = 24;
+            this.dataCustomer.Size = new System.Drawing.Size(240, 150);
+            this.dataCustomer.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of Products";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(572, 16);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(572, 116);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(177, 76);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // txtProducts
+            // 
+            this.txtProducts.Location = new System.Drawing.Point(572, 63);
+            this.txtProducts.Name = "txtProducts";
+            this.txtProducts.Size = new System.Drawing.Size(100, 22);
+            this.txtProducts.TabIndex = 7;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(386, 223);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(122, 23);
+            this.saveBtn.TabIndex = 8;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(386, 268);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(122, 23);
+            this.searchBtn.TabIndex = 9;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            // 
+            // showMapBtn
+            // 
+            this.showMapBtn.Location = new System.Drawing.Point(386, 322);
+            this.showMapBtn.Name = "showMapBtn";
+            this.showMapBtn.Size = new System.Drawing.Size(122, 23);
+            this.showMapBtn.TabIndex = 10;
+            this.showMapBtn.Text = "Show Map";
+            this.showMapBtn.UseVisualStyleBackColor = true;
+            // 
+            // showDistBtn
+            // 
+            this.showDistBtn.Location = new System.Drawing.Point(386, 367);
+            this.showDistBtn.Name = "showDistBtn";
+            this.showDistBtn.Size = new System.Drawing.Size(122, 23);
+            this.showDistBtn.TabIndex = 11;
+            this.showDistBtn.Text = "Show Distance";
+            this.showDistBtn.UseVisualStyleBackColor = true;
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(12, 16);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(341, 374);
+            this.map.TabIndex = 12;
+            this.map.Zoom = 0D;
+            this.map.Load += new System.EventHandler(this.GMapControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 473);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.showDistBtn);
+            this.Controls.Add(this.showMapBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.txtProducts);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataCustomer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataCustomer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtProducts;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button showMapBtn;
+        private System.Windows.Forms.Button showDistBtn;
+        private GMap.NET.WindowsForms.GMapControl map;
+    }
+}
+
---
+++ RoutingMap/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+
+namespace RoutingMap
+{
+    public partial class Form1 : Form
+    {
+        string keyWord;
+        double d;
+        List<PointLatLng> points = new List<PointLatLng>();
+
+        //Database Connection String
+
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Craiegg\source\repos\RoutingMap\Database\CustomerDB.mdf;Integrated Security=True;Connect Timeout=30");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Saving Data to Database on Save button click
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+
+                // Opening a stored procedure command instance
+
+                SqlCommand sqlCmd = new SqlCommand("AddOrEdit", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                // Adding values from form into database
+
+                sqlCmd.Parameters.AddWithValue("@mode", "Add");
+                sqlCmd.Parameters.AddWithValue("@ContactID", "0");
+                sqlCmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@NumberOfProd", txtProducts.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Success");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+
+            // Closing connection on completion
+
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void GMapControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
